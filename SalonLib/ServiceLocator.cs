60d48a7d85b2f5e27c67b4e2deb0afdd41390adc
Slatronio <slavyankokorin@gmail.com>
2023-07-03 17:57:03 +0000

    Решал конфликт merge'й

--- SalonLib/ServiceLocator.cs
+++ SalonLib/ServiceLocator.cs
@@ -7,15 +7,9 @@ public class ServiceLocator
     {
         store = new Dictionary<string, Func<string>>();
         store.Add("Читать стихи", () => "В читальном зале");
-<<<<<<< HEAD
         store.Add("Петь романсы", () => "У рояля");
-<<<<<<< HEAD
-=======
         store.Add("Писать статьи", () => "В кабинете");
->>>>>>> 32ddca5 (Добавлен сервис 3)
-=======
         store.Add("Играть в карты", () => "За карточным столом");
->>>>>>> e4fed17 (Добавлен тест для сервиса 4)
     }
 
     public static string GetService(string key)
