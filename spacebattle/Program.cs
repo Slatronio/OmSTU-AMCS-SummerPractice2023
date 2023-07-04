8a48a5e059d20f71c2fa6eaeb99598317f12fb36
Slatronio <slavyankokorin@gmail.com>
2023-07-04 17:30:44 +0000

    Задание 7

---
+++ spacebattle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace spacebattle{
+
+    public class Move
+    {
+        public static double[] ConstantMotion(double x, double y, double sp_x, double sp_y, double MotionExist){
+            double [] newPos = new double[2];
+            if (MotionExist == 0 || x == double.NaN ) 
+            {
+                throw new Exception();
+            }
+            else
+            {
+                newPos[0] = x + sp_x;
+                newPos[1] = y + sp_y;
+            }
+            return newPos;
+        }
+    }
+}
