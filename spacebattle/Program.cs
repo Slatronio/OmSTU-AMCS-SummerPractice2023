7e3c1b60c6102b26348389639cb5bd996e3e4f59
Slatronio <slavyankokorin@gmail.com>
2023-07-05 07:46:36 +0000

    Spacebattle8 by Slatronio

--- spacebattle/Program.cs
+++ spacebattle/Program.cs
@@ -4,7 +4,8 @@ namespace spacebattle{
 
     public class Move
     {
-        public static double[] ConstantMotion(double x, double y, double sp_x, double sp_y, double MotionExist){
+        public static double[] ConstantMotion(double x, double y, double sp_x, double sp_y, double MotionExist)
+        {
             double [] newPos = new double[2];
             if (MotionExist == 0 || x == double.NaN ) 
             {
@@ -16,6 +17,22 @@ namespace spacebattle{
                 newPos[1] = y + sp_y;
             }
             return newPos;
+        }
+        public static double Fuel(double start_fuel, double fuelPerLine)
+        {
+            double end_fuel = 0;
+            if (start_fuel < fuelPerLine){
+                throw new Exception();
+            }
+            else{
+                end_fuel = start_fuel - fuelPerLine;
+            }
+            return end_fuel;
+        }
+        public static double Angle(double start_angle, double angle_speed)
+        {
+            double end_angle = start_angle+angle_speed;
+            return end_angle;
         }
     }
 }
--- spacebattletest/MoveTest.cs
+++ spacebattletest/MoveTest.cs
@@ -10,29 +10,38 @@ namespace spacebattletests{
          public class Spacebattle
          {
             private ScenarioContext _scenarioContext;
-            private double x;
-            private double y;
-            private double sp_x;
-            private double sp_y;
-            private double MotionExist;
+            private double x = 0;
+            private double y = 0;
+            private double sp_x = 0;
+            private double sp_y = 0;
+            private double MotionExist = 1;
             private bool exp;
             private double[] NewPos = new double[2];
+            private double start_fuel = 1;
+            private double FuelPerLine = 1;
+            private double end_fuel = 1;
+            private double start_angle = 1;
+            private double angle_speed = 1;
+            private double end_angle = 0;
             public Spacebattle(ScenarioContext input)
             {
                 _scenarioContext = input;
             }
+
             [Given(@"космический корабль находится в точке пространства с координатами \((.*), (.*)\)")]
             public void Input1(double k1, double k2)
             {
                 x = k1;
                 y = k2;
             }
+
             [Given(@"космический корабль, положение в пространстве которого невозможно определить")]
             public void Input2()
             {
                 x = double.NaN;
                 y = double.NaN;
             }
+
             [Given(@"имеет мгновенную скорость \((.*), (.*)\)")]
             public void Input3(double k1, double k2)
             {
@@ -40,36 +49,90 @@ namespace spacebattletests{
                 sp_y = k2;
                 MotionExist = 1;
             }
+
             [Given(@"скорость корабля определить невозможно")]
-            
             public void Input4()
             {
                 sp_x = double.NaN;
                 sp_y = double.NaN;
                 MotionExist = 0;
             }
+
             [Given(@"изменить положение в пространстве космического корабля невозможно")]
-            public void Input5(){
+            public void Input5()
+            {
                 x = 0;
                 y = 0;
                 sp_x = 0;
                 sp_y = 0;
                 MotionExist = 0;
             }
+
+            [Given(@"космический корабль имеет топливо в объеме (.*) ед")]
+            public void Input6(double k1)
+            {
+                start_fuel = k1;
+            }
+
+            [Given(@"имеет скорость расхода топлива при движении (.*) ед")]
+            public void Input7(double k1)
+            {
+                FuelPerLine = k1;
+            }
+
+            [Given(@"космический корабль имеет угол наклона (.*) град к оси OX")]
+            public void Input8(double k1)
+            {
+                start_angle = k1;
+            }
+
+            [Given(@"имеет мгновенную угловую скорость (.*) град")]
+            public void Input9(double k1)
+            {
+                angle_speed = k1;
+            }
+            [Given(@"космический корабль, угол наклона которого невозможно определить")]
+            public void Input10()
+            {
+                exp = false;
+            }
+
+            [Given(@"мгновенную угловую скорость невозможно определить")]
+            public void Input11()
+            {
+                exp = false;
+            }
+
+            [Given(@"невозможно изменить уголд наклона к оси OX космического корабля")]
+            public void Input12()
+            {
+                exp = false;
+            }
+
             [When(@"происходит прямолинейное равномерное движение без деформации")]
             public void check()
             {
                 try{
                 NewPos = spacebattle.Move.ConstantMotion(x, y, sp_x, sp_y, MotionExist);
+                end_fuel = spacebattle.Move.Fuel(start_fuel, FuelPerLine);
                 }
                 catch (Exception){
                     exp = false;
                 }
             }
+
+            [When(@"происходит вращение вокруг собственной оси")]
+            public void check2()
+            {
+                end_angle = spacebattle.Move.Angle(start_angle,angle_speed);
+            }
+
             [Then(@"возникает ошибка Exception")]
-            public void Error(){
+            public void Error()
+            {
                 Assert.False(exp);
             }
+
             [Then(@"космический корабль перемещается в точку пространства с координатами \((.*), (.*)\)")]
             public void UpdatePos(double k1, double k2)
             {
@@ -78,6 +141,18 @@ namespace spacebattletests{
                 {
                     Assert.Equal(NewPos[i],NP[i]);
                 }
+            }
+
+            [Then(@"новый объем топлива космического корабля равен (.*) ед")]
+            public void UpdateFuel(double k1)
+            {
+                Assert.Equal(end_fuel,k1);
+            }
+
+            [Then("угол наклона космического корабля к оси OX составляет (.*) град")]
+            public void UpdateAngle(double k1)
+            {
+                Assert.Equal(end_angle,k1);
             }
          }
     }
